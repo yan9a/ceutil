6ed84b90e6f3ad1992762c051032476288b1090d
uditp <udit.patel@invenco.com>
2023-08-21 17:04:48 +1200

    [FIX] Modify to sudo permnissions for pkg installation

--- ceutil.sh
+++ ceutil.sh
@@ -48,7 +48,7 @@ if [[ "$opt_sh" == "install" ]]; then
         # install required lib
         echo "Installing required lib ..."        
         sudo apt update \
-        && apt install --no-install-recommends --no-install-suggests -y build-essential cmake \
+        && sudo apt install --no-install-recommends --no-install-suggests -y build-essential cmake \
         libgtk-3-dev checkinstall libwxgtk3.0-gtk3-dev \
         git pkg-config libavcodec-dev libavformat-dev libswscale-dev libopencv-dev \
         libjsoncpp-dev apt-transport-https ca-certificates \
